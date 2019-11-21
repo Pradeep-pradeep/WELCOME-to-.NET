4a743154dfd1b6199179c010223ed7af5b0eb6cc
Pradeep T <52797144+Pradeep-pradeep@users.noreply.github.com>
2019-11-21 10:48:28 +0530

    Add files via upload

---
+++ Access.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public abstract class Shape
+{
+    public abstract void draw();
+    public abstract void add();
+}
+public class Rectangle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+
+    public override void add()
+    {
+
+    }
+}
+public abstract class Circle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing circle...");
+    }
+}
+
+public class Addition:Circle
+{
+    public override void add()
+    {
+        Console.WriteLine("Addition Classs");
+    }
+}
+public class TestAbstract
+{
+    public static void Main()
+    {
+        Shape s = new Rectangle();
+        s.draw();
+        Shape c = new Addition();
+        c.draw();
+        c.add();
+    }
+}
